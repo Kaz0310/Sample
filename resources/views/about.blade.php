726e85914b445333a50f70b7a56cbd1ec89afb1f
Kaz0310 <0310kazuma@gmail.com>
2021-03-01 17:45:30 +0900

    Vue.js update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,9 +11,7 @@
 <div style="text-align: center;">
     <h1>aboutページ テスト2</h1>
 </div>
-<div id="app-1">
-{{ message }}
-</div>
+<div id="app-1">@{{ message }}</div>
 <script>
 var app1 = new Vue({
   el: '#app-1',
