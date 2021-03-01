7793441eee5442ef12a6189233eecdba394a59a8
Kaz0310 <0310kazuma@gmail.com>
2021-03-01 17:10:03 +0900

    Vue test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,10 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 </head>
 <body>
 <div style="text-align: center;">
     <h1>aboutページ テスト2</h1>
 </div>
+<div id="app-1">{{ message }}</div>  <!-- {{ message }} が Vueデータに置換される -->
+
+<script>
+var app1 = new Vue({
+  el: '#app-1',                        /* #app-1 要素に対して Vue を適用する */
+  data: { message: 'Hello world!' }    /* message という名前のデータを定義する */
+})
+</script>
 </body>
 </html>
