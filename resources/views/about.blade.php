2e485c7fc0db5db160e82ad50cfa37abbe049a6c
Kaz0310 <0310kazuma@gmail.com>
2021-03-19 12:32:29 +0900

    test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 </head>
