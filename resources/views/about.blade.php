42ab90e1cebfd5ebdb0f0b6727b36f0f7f5645bb
Kaz0310 <0310kazuma@gmail.com>
2021-03-19 12:26:29 +0900

    test toggle

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,8 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 </head>
