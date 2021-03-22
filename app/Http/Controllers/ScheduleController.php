bc3829962d02be42ed723da6f76ba02d502a679c
Kaz0310 <0310kazuma@gmail.com>
2021-03-22 15:07:10 +0900

    test

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -8,14 +8,14 @@ class ScheduleController extends Controller
 {
     public function arrive(){
 
-        DB::table('schedule')->where(["user_id" => 1])
-        ->insert(["arrive" => 1]);
+        DB::table('schedule')->where('user_id', 1)
+        ->update(["arrive" => 1]);
         return view('about');
     }
 
     public function leave(){
-        DB::table('schedule')->where(["user_id" => 1])
-        ->insert(["arrive" => 0]);
+        DB::table('schedule')->where('user_id', 1)
+        ->update(["arrive" => 0]);
         return view('about');
     }
 }
