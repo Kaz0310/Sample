5a2f4f231cc8a4f69abf963943c1a8b60a052bcc
Kaz0310 <0310kazuma@gmail.com>
2021-05-07 19:10:30 +0900

    add table

--- resources/views/user/job.blade.php
+++ resources/views/user/job.blade.php
@@ -13,12 +13,12 @@
     <table>
         <tbody>
             <tr>
-            <th rowspan="2" width="50"></th>
-            <th rowspan="2" width="50">期間</th>
-            <th scope="row" width="300">案件名</th>
+            <th class="center" rowspan="2" width="50"></th>
+            <th class="center" rowspan="2" width="50">期間</th>
+            <th class="center" scope="row" width="300">案件名</th>
             </tr>
             <tr>
-            <th scope="row" width="300">業務内容</th>
+            <th class="center" scope="row" width="300">業務内容</th>
             </tr>
             <?php foreach($career as $val){ ?>
             <tr>
