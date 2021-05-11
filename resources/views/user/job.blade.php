2c51bc81fd72dda359f2416fac14e1db9e37111d
Kaz0310 <0310kazuma@gmail.com>
2021-05-11 13:14:51 +0900

    add table

--- resources/views/user/job.blade.php
+++ resources/views/user/job.blade.php
@@ -16,7 +16,7 @@
             <th class="center" rowspan="2" width="50"></th>
             <th class="center" rowspan="2" width="50">期間</th>
             <th class="center" scope="row" width="300">案件名</th>
-            <th class="center" rowspan="2" width="70">系統・規模　役割</th>
+            <th class="center" rowspan="2" colspan="2" width="60">系統・規模　役割</th>
             <th class="center" rowspan="2" width="100">担当工程</th>
             <th class="center" rowspan="2" width="100">使用ツール等</th>
             </tr>
@@ -29,16 +29,16 @@
                 <td rowspan="4">yyyy/mm - yyyy/mm</td>
                 <td><?php echo $val->matter; ?></td>
                 <td colspan="2">開発系</td>
+                <td colspan="1" width="40">人月</td>
+                <td colspan="1">１</td>
+                <td colspan="1" width="40">メンバ数</td>
+                <td colspan="1">１</td>
+                <td colspan="2">リーダー</td>
                 <td rowspan="4"></td>
                 <td rowspan="4"></td>
             </tr>
             <tr rowspan="3">
                 <td rowspan="3"><?php echo $val->detail; ?></td>
-                <td colspan="1">人月</td>
-                <td colspan="1">１</td>
-                <td colspan="1">メンバ数</td>
-                <td colspan="1">１</td>
-                <td colspan="2">リーダー</td>
             </tr>
             <?php } ?>
         </tbody>
