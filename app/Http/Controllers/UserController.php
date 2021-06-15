ab4f4e778a521dff1316a519acd2adb32ab9d156
Kaz0310 <0310kazuma@gmail.com>
2021-06-15 10:10:15 +0900

    add exp

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -173,7 +173,7 @@ class UserController extends Controller
       //$industry_data = DB::table('industry')->leftJoin('experience', 'code', '=', 'experience.industry_code')->where('number', $data1['number'])->orWhereNull('number')->orderBy('code', 'asc')->get();
       
       $subSQL = DB::table('experience')->where('number', $data1['number'])->toSQL();
-      $industry_data = DB::table('industry')->leftJoinSub($subSQL, 'exp', 'industry.code','exp.industry_code')->orderBy('code', 'asc')->get();
+      $industry_data = DB::table('industry')->leftJoinSub($subSQL, 'exp', 'industry.code', 'exp.industry_code')->get();
       return view('user.exp',['data' => $user_data],['industry' => $industry_data]);
     }
 }
