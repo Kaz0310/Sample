1ab19d7ea54eabb90178d08816c1c5da5a146855
Kaz0310 <0310kazuma@gmail.com>
2021-06-28 14:59:23 +0900

    add skill

--- resources/views/user/skill.blade.php
+++ resources/views/user/skill.blade.php
@@ -28,27 +28,27 @@
                 <tbody>
                 <tr>
                     <th class="left" scope="row" width="120">レベル4</th>
-                    <td class="left" width="420"></td>
+                    <td class="left" width="420">対象となる技術の経験を体系化し先進的なやり方を</br>工夫・開発できる</td>
                 </tr>
                 <tr>
                     <th class="left" scope="row" width="120">レベル3</th>
-                    <td class="left" width="420"></td>
+                    <td class="left" width="420">他のエンジニアに対して指導及び環境を提供できる</br>作業標準化またはチェックリスト化できる</td>
                 </tr>
                 <tr>
                     <th class="left" scope="row" width="120">レベル2</th>
-                    <td class="left" width="420"></td>
+                    <td class="left" width="420">サポートがなくても自律的に実施できる</td>
                 </tr>
                 <tr>
                     <th class="left" scope="row" width="120">レベル1</th>
-                    <td class="left" width="420"></td>
+                    <td class="left" width="420">サポートがあれば実施できる</br>トレーニングを受けた程度の知識はある</td>
                 </tr>
                 <tr>
                     <th class="left" scope="row" width="120">レベル0</th>
-                    <td class="left" width="420"></td>
+                    <td class="left" width="420">知識、経験なし</td>
                 </tr>
                 <tr>
                     <th class="left" scope="row" width="120">未経験参加希望</th>
-                    <td class="left" width="420"></td>
+                    <td class="left" width="420">知識、経験ともに無いが機会があれば参加したい</td>
                 </tr>
                 </tbody>
             </table>
