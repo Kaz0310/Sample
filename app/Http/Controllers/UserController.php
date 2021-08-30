fec4cfc05b3ccce7d1a94b1042ead4bfc71779d9
Kaz0310 <0310kazuma@gmail.com>
2021-08-30 13:48:22 +0900

    add search

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -223,7 +223,7 @@ class UserController extends Controller
       $technology_data = DB::table('technology')->Join('technology_class','code','=','technology_code')->orderBy('technology_class.technology_code', 'asc')->orderBy('technology_class.technology_class_code', 'asc')->get();
 
       $subSQL = DB::table('business_experience')->where('experience_code', '=', '1')->where('experience_class_code', '=', '7')->where('level', '>', '2')->toSQL();
-      $user_data = DB::table('employee')->leftJoinSub($subSQL, 'bus', 'employee.number', 'bus.number')->get();
+      $user_data = DB::table('employee')->leftJoinSub($subSQL, 'bus', 'number', 'bus.number')->get();
 
       return view('user.search',['data' => $user_data, 'business' => $business_data, 'technology' => $technology_data]);
     }
